1efcb85f0f90b67f141cbb3ee3ad7b49428c4458
Ragan <raganalemu3@gmail.com>
2022-10-01 14:02:21 +0300

    added validation file

--- routes/auth.js
+++ routes/auth.js
@@ -2,18 +2,7 @@ const router = require("express").Router();
 const User = require("../model/User");
 
 const { registerValidation } = require("../validation");
-// const Joi = require("@hapi/joi");
-
-// Validation
-// const schema = Joi.object({
-//   name: Joi.string().min(4).required(),
-//   email: Joi.string().min(6).required().email(),
-//   password: Joi.string().min(6).required(),
-// });
-
 router.post("/register", async (req, res) => {
-  // const Validation=schema.validate(req.body);
-  // const { value, error } = schema.validate(req.body);
   const { value, error } = registerValidation(req.body);
   if (error) {
     res.status(400).send(error.message);
