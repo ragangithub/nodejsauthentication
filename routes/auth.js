67a58896fc456bb95625658c61af854ea8ec9422
Ragan <raganalemu3@gmail.com>
2022-10-01 16:13:45 +0300

    hashed password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 const { registerValidation } = require("../validation");
 router.post("/register", async (req, res) => {
@@ -12,11 +13,16 @@ router.post("/register", async (req, res) => {
     if (emailExists) {
       return res.send("Email exists");
     }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     //Create new user
     const user = new User({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     });
     try {
       const savedUser = await user.save();
