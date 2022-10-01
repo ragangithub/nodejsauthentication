4bf0815cedacf1295c6f6b35c0f713a6a94e9f83
Ragan <raganalemu3@gmail.com>
2022-10-01 12:05:18 +0300

    connected to database

--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,5 @@ const router = require("express").Router();
 router.post("/register", (req, res) => {
   res.send("Register");
 });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//Connect Db
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 //Import Routes
 const authRoute = require("./routes/auth");
 
 //Route Middlewares
-app.use("api/user", authRoute);
+app.use("/api/user", authRoute);
 
 app.listen(3000);
